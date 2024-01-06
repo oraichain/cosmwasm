3892d7a59eea0ecf19d27ed12ceb88e655577dc9
Simon Warta <simon@warta.it>
2024-01-06 14:16:32 +0100

    fix leak memory

--- packages/vm/src/modules/cached_module.rs
+++ packages/vm/src/modules/cached_module.rs
@@ -1,8 +1,12 @@
-use wasmer::Module;
+use wasmer::{Engine, Module};
 
 #[derive(Debug, Clone)]
 pub struct CachedModule {
     pub module: Module,
+    /// The runtime engine to run this module. Ideally we could use a single engine
+    /// for all modules but the memory issue described in <https://github.com/wasmerio/wasmer/issues/4377>
+    /// requires using one engine per module as a workaround.
+    pub engine: Engine,
     /// The estimated size of this element in memory.
     /// Since the cached modules are just [rkyv](https://rkyv.org/) dumps of the Module
     /// instances, we use the file size of the module on disk (not the Wasm!)
